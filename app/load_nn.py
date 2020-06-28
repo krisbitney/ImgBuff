a065807e5a99df75bd2ab5aefe29dbcb986cd54e
krisbitney <39842820+krisbitney@users.noreply.github.com>
2020-06-28 13:58:40 -0700

    reworked how neural net is loaded

--- app/load_nn.py
+++ app/load_nn.py
@@ -74,4 +74,4 @@ def refresh_gan(version, crit_thresh=0.65, loss_weights=(1.,50.), bs=1, size=320
     gan.path = Path('/app/')
     gan.model_dir = Path('/app/')
     gan.load(version)
-    return generator
+    return gan
